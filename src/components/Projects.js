ba4e78466b7361e9eae687038a91d4fecb8f0b3d
whatkimkong <whatkimkong@gmail.com>
2022-02-01 11:31:57 +0100

    add: project props structure

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,8 @@
-import { Flex, Heading, useColorMode } from '@chakra-ui/react';
-import Carousel from 'react-multi-carousel';
-import Image from './Image';
+import { Flex, Heading, useColorMode, Text } from "@chakra-ui/react";
+import Image from "./Image";
+import { useRouter } from "next/router";
 
-export const Projects = () => {
+export const Projects = ({title, description, image}) => {
   const projects = [
     {
       title: "You're on one already!",
@@ -11,31 +11,31 @@ export const Projects = () => {
         "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     },
     {
-      title: "",
+      title: "Howdiy",
       description: "",
       image:
         "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     },
     {
-      title: "",
+      title: "KanKan",
       description: "",
       image:
         "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     },
     {
-      title: "",
+      title: "Haiku",
       description: "",
       image:
         "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     },
     {
-      title: "",
+      title: "Empress Voroti",
       description: "",
       image:
         "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     },
     {
-      title: "",
+      title: "Devotox",
       description: "",
       image:
         "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
@@ -44,24 +44,29 @@ export const Projects = () => {
 
   const router = useRouter();
   const { id } = router.query;
-  const p = projects[id];
 
-    const { colorMode } = useColorMode()
-  const color = { light: 'tomato', dark: 'orange.300' }
+  const { colorMode } = useColorMode();
+  const color = { light: "tomato", dark: "orange.300" };
 
   return (
-  <Flex justifyContent="center" alignItems="center" flexDirection="column" height="50vh">
-    <Heading
-      fontSize="4vw"
-      color={color[colorMode]}
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      height="50vh"
     >
-      {title}
-    </Heading>
-    <Image key={images[0]} url={images[0]} alt={images[0]} />
-  </Flex>
-  )
-}
+      <Heading fontSize="4vw" color={color[colorMode]}>
+        {title}
+      </Heading>
+      <Image key={id} url={image} alt={image} />
+      <Text>{description}</Text>
+    </Flex>
+  );
+};
 
 Projects.defaultProps = {
-  title: 'Project',
-}
+  title: "You're on one already!",
+  description: "",
+  image:
+    "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+};
